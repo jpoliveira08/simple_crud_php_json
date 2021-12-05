fe675197cbfbbf6a05f48a0750a4d1ea4113812b
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2021-12-05 11:50:45 -0300

    Implemented some validations in create

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -2,14 +2,13 @@
 namespace App\Controller;
 
 use App\Models\User;
-
+@session_start();
 class UserController extends Controller
 {
     //Meio de campo
     /**
      * Listar – O controller deve buscar com a Model uma lista de contatos e enviar para View.
      */
-
     public function toList()
     {
         $users = User::getUsers();
@@ -33,34 +32,58 @@ class UserController extends Controller
         if($_POST){
             User::updateUser($_POST, $user['id']);
         }
-        header('Location: ?controller=App\Controller\UserController&method=toList');
-        exit;
+        $this->redirectToList();
     }
 
     public function create()
     {
+        session_destroy();
         return $this->view('./public/form');
     }
     
     public function save()
     {
+        $isValid = true;
         if($_POST){
-            $name = $_POST['name'];
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $phone = $_POST['phone'];
-            $website = $_POST['website'];
-
+            if(!$_POST['name']){
+                $_SESSION['error']['name'] = 'Name is mandatory';
+                $isValid = false;
+            }
+            if(!$_POST['username'] || strlen($_POST['username']) < 6 || strlen($_POST['username']) > 16){
+                $_SESSION['error']['username'] = 'Username is required and it must be more than 6 and less then 16 characters';
+                $isValid = false;
+            }
+            if($_POST['email'] && !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                $_SESSION['errors']['email'] = 'This must be a valid email address';
+                $isValid = false;
+            }
+            if(!filter_var($_POST['phone'], FILTER_VALIDATE_INT)){
+                $_SESSION['errors']['phone'] = 'This must be a valid phone number';
+                $isValid = false;
+            }
+            if(!$isValid){
+                $this->redirectToForm();
+            }
             User::createUser($_POST);
+            
         }
-        header('Location: ?controller=App\Controller\UserController&method=toList');
-        exit;
+        session_destroy();
+        $this->redirectToList();
     }
 
     public function delete()
     {
         $user = User::getUserById($_POST['id']);
         User::deleteUser($user['id']);
+        $this->redirectToList();
+    }
+
+    private function redirectToForm(){
+        header('Location: ?controller=App\Controller\UserController&method=create');
+        exit;
+    }
+
+    private function redirectToList(){
         header('Location: ?controller=App\Controller\UserController&method=toList');
         exit;
     }
--- public/form.php
+++ public/form.php
@@ -2,19 +2,23 @@
     <form method="POST" action="?controller=App\Controller\UserController&<?php echo isset($user['id']) ? "method=update&id={$user['id']}" : "method=save"; ?>" >
         <div class="form-group">
             <label>Name</label>
-            <input name="name" value="<?php echo isset($user['name']) ? $user['name'] : null; ?>" class="form-control">
+            <input name="name" value="<?php echo isset($user['name']) ? $user['name'] : null; ?>" class="form-control <?php echo isset($_SESSION['error']['name']) ? 'is-invalid' : '' ?>">
+            <div class="invalid-feedback"><?php echo $_SESSION['error']['name'] ?? ''; ?></div>
         </div>
         <div class="form-group">
             <label>Username</label>
-            <input name="username" value="<?php echo isset($user['username']) ? $user['username'] : null; ?>" class="form-control">
+            <input name="username" value="<?php echo isset($user['username']) ? $user['username'] : null; ?>" class="form-control <?php echo isset($_SESSION['error']['username']) ? 'is-invalid' : '' ?>">
+            <div class="invalid-feedback"><?php echo $_SESSION['error']['username'] ?? ''; ?></div>
         </div>
         <div class="form-group">
             <label>Email</label>
-            <input name="email" value="<?php echo isset($user['email']) ? $user['email'] : null; ?>" class="form-control">
+            <input name="email" value="<?php echo isset($user['email']) ? $user['email'] : null; ?>" class="form-control <?php echo isset($_SESSION['errors']['email']) ? 'is-invalid' : '' ?>">
+            <div class="invalid-feedback"><?php echo $_SESSION['errors']['email'] ?? ''; ?></div>
         </div>
         <div class="form-group">
             <label>Phone</label>
-            <input name="phone" value="<?php echo isset($user['phone']) ? $user['phone'] : null; ?>" class="form-control">
+            <input name="phone" value="<?php echo isset($user['phone']) ? $user['phone'] : null; ?>" class="form-control <?php echo isset($_SESSION['errors']['phone']) ? 'is-invalid' : '' ?>">
+            <div class="invalid-feedback"><?php echo $_SESSION['errors']['phone'] ?? ''; ?></div>
         </div>
         <div class="form-group">
             <label>Website</label>
